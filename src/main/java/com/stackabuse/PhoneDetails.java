2d7260ac80a0ce676aaadff756b6d48aae7943e5
Robley <robleyadrian@gmail.com>
2019-04-10 00:24:05 +0300

    update PhoneDetails class to include manufacturer info

--- src/main/java/com/stackabuse/PhoneDetails.java
+++ src/main/java/com/stackabuse/PhoneDetails.java
@@ -3,7 +3,7 @@ package com.stackabuse;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonPropertyOrder({ "internal_memory", "display_size", "phone_name" })
+@JsonPropertyOrder({ "internal_memory", "display_size", "phone_name", "manufacturer" })
 public class PhoneDetails {
   @JsonProperty("phone_name")
   private String name;
@@ -11,57 +11,76 @@ public class PhoneDetails {
   private String displaySize;
   @JsonProperty("internal_memory")
   private String memory;
+  @JsonProperty("manufacturer")
+  private Manufacturer manufacturer;
 
   public PhoneDetails() {
     // default contructor
   }
 
-  public PhoneDetails(String name, String displaySize, String memory) {
+  public PhoneDetails(String name, String displaySize, String memory, Manufacturer manufacturer) {
     this.name = name;
     this.displaySize = displaySize;
     this.memory = memory;
+    this.manufacturer = manufacturer;
   }
 
-  /**
-   * @return String return the name
-   */
-  public String getName() {
-    return name;
-  }
 
-  /**
-   * @param name the name to set
-   */
-  public void setName(String name) {
-    this.name = name;
-  }
 
-  /**
-   * @return String return the displaySize
-   */
-  public String getDisplaySize() {
-    return displaySize;
-  }
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
 
-  /**
-   * @param displaySize the displaySize to set
-   */
-  public void setDisplaySize(String displaySize) {
-    this.displaySize = displaySize;
-  }
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  /**
-   * @return String return the memory
-   */
-  public String getMemory() {
-    return memory;
-  }
+    /**
+     * @return String return the displaySize
+     */
+    public String getDisplaySize() {
+        return displaySize;
+    }
 
-  /**
-   * @param memory the memory to set
-   */
-  public void setMemory(String memory) {
-    this.memory = memory;
-  }
+    /**
+     * @param displaySize the displaySize to set
+     */
+    public void setDisplaySize(String displaySize) {
+        this.displaySize = displaySize;
+    }
+
+    /**
+     * @return String return the memory
+     */
+    public String getMemory() {
+        return memory;
+    }
+
+    /**
+     * @param memory the memory to set
+     */
+    public void setMemory(String memory) {
+        this.memory = memory;
+    }
+
+    /**
+     * @return Manufacturer return the manufacturer
+     */
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+
+    /**
+     * @param manufacturer the manufacturer to set
+     */
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
 
 }
